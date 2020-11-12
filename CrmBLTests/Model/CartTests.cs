44f71a995db45267fe19244edf999e7abb79264e
Андрей <andrei.bagan2@mail.ru>
2020-11-12 20:04:58 +0300

    Добавлены модульные тесты, создан класс "cart"

---
+++ CrmBLTests/Model/CartTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrmBL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBL.Model.Tests
+{
+    [TestClass()]
+    public class CartTests
+    {
+        [TestMethod()]
+        public void CartTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetEnumeratorTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
